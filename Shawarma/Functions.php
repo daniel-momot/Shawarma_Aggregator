af2384b2af594443f56d40d44cfd7e6d4d6b3101
zeytunyan <47988040+zeytunyan@users.noreply.github.com>
2019-12-03 23:22:17 +0300

    Add files via upload

--- Shawarma/Functions.php
+++ Shawarma/Functions.php
@@ -22,11 +22,14 @@ function make_table($query) {
   return $table;
 }
 
-function make_select($select_name, $disabled_string, $query_res = false, $isRequired = true) {
+function make_select($select_name, $query_res = false,  $isRequired = true, $disabled_string = '') {
   $required = $isRequired ? 'required' : '';
-  $select = "<select name=\"$select_name\" $required>
-             <option value=\"\" disabled selected>$disabled_string</option>";
-
+  $select = "<select name=\"$select_name\" $required>";
+  
+  if ($disabled_string) {
+    $select += "<option value=\"\" disabled selected>$disabled_string</option>";
+  }
+  
   if($query_res) {
     foreach($query_res as $query_row) {
       $select .= "<option value=\"$query_row[0]\"> $query_row[1] </option>";
