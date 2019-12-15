14a34b1565131401a5dfe939ce8ff255738c4374
Daniel Momot <36703311+daniel-momot@users.noreply.github.com>
2019-12-15 16:15:08 +0300

    Update add_outlet_shawarma_price.php

--- Shawarma/add_outlet_shawarma_price.php
+++ Shawarma/add_outlet_shawarma_price.php
@@ -5,10 +5,6 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Shawarma with price in Outlets</title>
-	<?php
-		include 'Connect.php';
-		include 'Functions.php';
-	?>
 
 </head>
 
@@ -21,15 +17,22 @@
 
 		Outlet name:
 		<?php
-		  $sel_query = $pdo->query("SELECT ID, Outlet_name FROM outlets ORDER BY Outlet_name");
-		  echo make_select('outlet_select', $sel_query);
+			include 'Connect.php'; // create connection to database 
+			include 'Functions.php'; // import functions of database abstraction level
+	    
+	    		// create query for retrieving data for HTML-style <select> for outlets
+			$sel_query = $pdo->query("SELECT ID, Outlet_name FROM outlets ORDER BY Outlet_name");	    
+	    		// displaying HTML-style <select> for outlets
+			echo make_select('outlet_select', $sel_query);
 		?>
 		
 		<br /><br />
 		Shawarma:
 		<?php
-		  $sel_query = $pdo->query("SELECT ID, Type FROM shawarma ORDER BY Type");
-		  echo make_select('shawarma_select', $sel_query);
+	    		// create query for retrieving data for HTML-style <select> for shawarma types
+			$sel_query = $pdo->query("SELECT ID, Type FROM shawarma ORDER BY Type");
+	    		// displaying HTML-style <select> for shawarma types
+			echo make_select('shawarma_select', $sel_query);
 		?>
 		
 		<br /><br />
@@ -42,24 +45,32 @@
 	
 
 <?php
-  
-  if(isset($_POST['outlet_select']) && isset($_POST['shawarma_select']) && isset($_POST['price'])) {
-    
-	$query = $pdo->query("INSERT INTO `outlet_shawarma_price`(`Outlet_ID`, `Shawarma_ID`, `Price`) VALUES (" . $_POST['outlet_select'].", " . $_POST['shawarma_select'] . ", " . $_POST['price'] . ")");
-   
-    if(!$query) {
-		$res = $pdo->errorCode();
-		if ($res == 23000) {
-			echo 'Price for this shawarma in this outlet is already set';
-		}
-		else {
-			echo 'Database query failed: (' . $pdo->errorCode() . ') ' . $pdo->errorInfo()[2];
-		}
-    } else {
-		echo 'Price successfully set!';
-	}   
-  }
- ?>
+	  // this part of file contains logics for processing data from form
+	  
+	  // if all need data is set
+	  if(isset($_POST['outlet_select']) && isset($_POST['shawarma_select']) && isset($_POST['price'])) {
+		  // create query for inserting a row
+		  $query = $pdo->query("INSERT INTO `outlet_shawarma_price`(`Outlet_ID`, `Shawarma_ID`, `Price`) VALUES (" . $_POST['outlet_select'].", " . $_POST['shawarma_select'] . ", " . $_POST['price'] . ")");
+		  
+		  // if query fails
+		  if(!$query) {
+			  // get arrer code
+			  $res = $pdo->errorCode();
+			  // if query fails because of unique index violation, display it
+			  if ($res == 23000) {
+				  echo 'Price for this shawarma in this outlet is already set';
+			  }
+			  // if other error occured, show error code and message
+			  else {
+				  echo 'Database query failed: (' . $res . ') ' . $pdo->errorInfo()[2];
+			  }
+		  }
+		  // if query was successful, display it
+		  else {
+			  echo 'Price successfully set!';
+		  }   
+	  }
+?>
   </center>
   <a href ="index.html">Back</a>
 </body>
